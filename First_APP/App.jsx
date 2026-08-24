9c1b57fc26bd820fab48d148eee182d28abea527
vishal <vishalcode247@gmail.com>
2026-08-24 07:52:49 +0530

    styling part done

--- First_APP/App.jsx
+++ First_APP/App.jsx
@@ -1,81 +1,73 @@
-import React from 'react';
-import {
-  Text,
-  View,
-  Image,
-  Button,
-  TouchableOpacity,
-  Alert,
-  TouchableHighlight,
-  Pressable,
-  SafeAreaView,
-  StyleSheet,
-} from 'react-native';
+import React from 'react'
+import { Text, StyleSheet, View, ScrollView } from 'react-native'
 
-App = () => {
+
+const App = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>Hello</Text>
+    <ScrollView
+      contentContainerStyle={{gap:10}}
+      // horizontal
+      style={styles.container}>
+      <View style={styles.box1}></View>
+      <View style={styles.box2}></View>
+      <View style={styles.box3}></View>
+      <View style={styles.box1}></View>
+      <View style={styles.box2}></View>
+      <View style={styles.box3}></View>
+      <View style={styles.box1}></View>
+      <View style={styles.box2}></View>
+      <View style={styles.box3}></View>
+      <View style={styles.box1}></View>
+      <View style={styles.box2}></View>
+      <View style={styles.box3}></View>
+      <View style={styles.box1}></View>
+      <View style={styles.box2}></View>
+      <View style={styles.box3}></View>
+      <View style={styles.box1}></View>
+      <View style={styles.box2}></View>
+      <View style={styles.box3}></View>
+      <View style={styles.box1}></View>
+      <View style={styles.box2}></View>
+      <View style={styles.box3}></View>
+      <View style={styles.box1}></View>
+      <View style={styles.box2}></View>
+      <View style={styles.box3}></View>
+      <View style={styles.box1}></View>
+      <View style={styles.box2}></View>
+      <View style={styles.box3}></View>
+    </ScrollView>
+  )
+}
 
-      <Image
-        style={{ width: 200, height: 300 }}
-        source={{
-          uri: 'https://images.unsplash.com/photo-1787240663846-598e1033a919?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        }}
-      />
+export default App
 
-      {/* <Button title="Press me"></Button>
-
-      <TouchableOpacity
-        style={{ padding: 10, backgroundColor: 'green' }}
-        onPress={() => Alert.alert('Pressed !!')}
-      >
-        <Text>Button</Text>
-      </TouchableOpacity>
-
-      <TouchableHighlight
-        style={{ padding: 10, backgroundColor: 'red' }}
-        onPress={() => Alert.alert('Pressed !!')}
-      >
-        <Text>Hello</Text>
-      </TouchableHighlight> */}
-
-      <Pressable style={styles.button}>
-        <Text style={styles.btnText}>Press me</Text>
-      </Pressable>
-    </SafeAreaView>
-  );
-};
-
-export default App;
 
 const styles = StyleSheet.create({
   container: {
-    width: '100%',
-    height: '100%',
-    padding: '10',
-    paddingTop: '35',
-    backgroundColor: 'black',
-    gap:10,
-  },
-  text: {
-    color: 'white',
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  button: {
+    paddingTop:50,
     padding: 10,
-    backgroundColor: 'yellow',
-    width: '150',
-    margin: '20',
-    justifyContent:"center",
-    alignItems:"center",
-    borderRadius:50,
-    borderWidth:2,
-    borderBottomColor:"red"
+    flex: 1,
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    backgroundColor: 'black'
   },
-  btnText: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  box1: {
+    width: 80,
+    height: 80,
+    backgroundColor: "red",
+    borderRadius:"50%"
   },
-});
+  box2: {
+    width: 80,
+    height: 80,
+    backgroundColor: "yellow",
+    borderRadius:"50%"
+  },
+  box3: {
+    width: 80,
+    height: 80,
+    backgroundColor: "green",
+    borderRadius:"50%"
+  }
+
+})
