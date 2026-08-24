674fd87ca047bd91d87ae99fa2e36eedeca077f8
vishal <vishalcode247@gmail.com>
2026-08-24 06:22:22 +0530

    styling part done

--- First_APP/App.jsx
+++ First_APP/App.jsx
@@ -9,12 +9,13 @@ import {
   TouchableHighlight,
   Pressable,
   SafeAreaView,
+  StyleSheet,
 } from 'react-native';
 
 App = () => {
   return (
-    <SafeAreaView>
-      <Text> Hello </Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Hello</Text>
 
       <Image
         style={{ width: 200, height: 300 }}
@@ -23,7 +24,7 @@ App = () => {
         }}
       />
 
-      <Button title="Press me"></Button>
+      {/* <Button title="Press me"></Button>
 
       <TouchableOpacity
         style={{ padding: 10, backgroundColor: 'green' }}
@@ -37,14 +38,44 @@ App = () => {
         onPress={() => Alert.alert('Pressed !!')}
       >
         <Text>Hello</Text>
-      </TouchableHighlight>
+      </TouchableHighlight> */}
 
-      <Pressable style={{padding:10, backgroundColor:"yellow"}}>
-        <Text>Press me</Text>
+      <Pressable style={styles.button}>
+        <Text style={styles.btnText}>Press me</Text>
       </Pressable>
     </SafeAreaView>
   );
 };
 
-
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+    padding: '10',
+    paddingTop: '35',
+    backgroundColor: 'black',
+    gap:10,
+  },
+  text: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  button: {
+    padding: 10,
+    backgroundColor: 'yellow',
+    width: '150',
+    margin: '20',
+    justifyContent:"center",
+    alignItems:"center",
+    borderRadius:50,
+    borderWidth:2,
+    borderBottomColor:"red"
+  },
+  btnText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
